249fce10e8eb5e99e9fd415b768ed93c49f15ba9
Brok3Turtl3 <brok3turtl3@gmail.com>
2023-02-26 07:40:18 -0500

    Login Component added

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='flex flex-row flex-wrap justify-around'><p className='p-3'>SalesHive &copy; 2023</p><p className='p-3'>Contact Us</p></div>
   )
 }
 
--- client/src/components/HeaderNav.js
+++ client/src/components/HeaderNav.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
+import LoginForm from './LoginForm';
+
 
 const HeaderNav = () => {
+
+const [showLogin, setShowLogin] = useState(false)
+
+
+const handleLoginClick = () => {
+	setShowLogin(true)
+}
+
+const handleClose = () => {
+	setShowLogin(false)
+}
+
+
+
+
 	return (
 		<div className='flex items-center flex-col sm:flex-row justify-between border-b-2 border-blue-500'>
+			{showLogin && <LoginForm onClose={handleClose}/>}
 			<div className='flex flex-row items-center pt-3 pb-3'>
-				<div className='text-5xl sm:text-6xl p-4'>Sales Hive</div>
+				<div className='text-5xl sm:text-6xl p-4 '>Sales Hive</div>
 				
 					<img className='w-16 sm:w-24' src='./images/honey.svg' alt='' />
 				
@@ -12,8 +30,8 @@ const HeaderNav = () => {
 
 			<nav>
 				<ul className='flex flex-row'>
-					<li className='p-4'>Sign Up</li>
-					<li className='p-4'>Log In</li>
+					<li className='p-4 text-yellow-200 text-xl' >Sign Up</li>
+					<li className='p-4 text-yellow-200 text-xl' onClick={handleLoginClick}>Log In</li>
 					<li></li>
 				</ul>
 			</nav>
